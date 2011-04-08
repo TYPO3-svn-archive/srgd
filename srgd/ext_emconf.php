3aecd055bec7c7d25005f0a3aa6fce4bd41a031a
sruegg <sruegg@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-08 12:59:04 +0000

    last version 0.0.3 starts the repository.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/srgd/trunk@46372 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ srgd/ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "srgd".
+#
+# Auto generated 19-03-2011 17:45
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'SR GD Crop & Resize',
+	'description' => 'Crop and Resize with GD. Usefull when ImageMagick is not available on the hoster.
+            Idea from jb_gd_resize Typo3 extension and Smart Image Resizer script.
+            Report to web@sruegg.ch',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '0.0.3',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Sebastien Ruegg',
+	'author_email' => 'web@sruegg.ch',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:4:{s:29:"class.ux_tslib_gifBuilder.php";s:4:"6a48";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"c2d8";s:10:"thumbs.php";s:4:"9ce8";}',
+	'suggests' => array(
+	),
+);
+
+?>
